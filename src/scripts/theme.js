e24611651372c1f51189ac0661ecf3706ca437de
victor-avancini <victor.avancini@hotmail.com>
2023-11-07 21:17:58 -0300

    Tarefa 1 concluída

--- src/scripts/theme.js
+++ src/scripts/theme.js
@@ -1,1 +1,26 @@
-/* Desenvolva sua lógica aqui ... */
+const darkMode = () =>{
+    const headerBtn = document.querySelector(".header__btn")
+
+    headerBtn.addEventListener("click", () => {
+        const html = document.querySelector("html")
+
+        const DarkMode = html.classList.toggle("dark-mode")
+        headerBtn.classList.toggle("header__btn--dark-mode")
+        
+        localStorage.setItem("@dark-mode", DarkMode)
+    })
+}
+
+const verifyMode = () => {
+    const darkInfo = JSON.parse(localStorage.getItem("@dark-mode"))
+    const headerBtn = document.querySelector(".header__btn")
+    const html = document.querySelector("html")
+
+    if(darkInfo){
+        html.classList.add("dark-mode")
+        headerBtn.classList.add("header__btn--dark-mode")
+    }
+}
+
+darkMode()
+verifyMode()
